c7ce372cb8f9d74b676d875a3934f484f603b2db
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 19:32:41 +0700

    add: Hello World

--- src/main/java/com/safetypin/payment/PaymentApplication.java
+++ src/main/java/com/safetypin/payment/PaymentApplication.java
@@ -2,6 +2,9 @@ package com.safetypin.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PaymentApplication {
@@ -10,4 +13,13 @@ public class PaymentApplication {
 		SpringApplication.run(PaymentApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+	@GetMapping
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
