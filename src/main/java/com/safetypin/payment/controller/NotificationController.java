81b26e3732092d184dc13580ee2fce08966e0384
riorio805 <sefrianojieftara@gmail.com>
2025-05-18 22:50:22 +0700

    feat: enhance NotificationController to handle payment notifications with signature verification

--- src/main/java/com/safetypin/payment/controller/NotificationController.java
+++ src/main/java/com/safetypin/payment/controller/NotificationController.java
@@ -1,20 +1,33 @@
 package com.safetypin.payment.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.safetypin.payment.service.MidtransSignatureVerifier;
+import com.safetypin.payment.service.TransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/payment/notification")
 public class NotificationController {
     private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
+
+    private final TransactionService transactionService;
+
+    private final MidtransSignatureVerifier midtransSignatureVerifier;
+
+    public NotificationController(
+            TransactionService transactionService,
+            MidtransSignatureVerifier midtransSignatureVerifier
+    ) {
+        this.transactionService = transactionService;
+        this.midtransSignatureVerifier = midtransSignatureVerifier;
+    }
 
     /**
      * Endpoint to receive notifications pushed from a web service.
@@ -23,12 +36,43 @@ public class NotificationController {
      * @return ResponseEntity indicating the result of processing the notification.
      */
     @PostMapping("/push")
-    public ResponseEntity<String> handleIncomingNotification(@RequestBody Object notificationPayload) {
+    public ResponseEntity<String> handleIncomingNotification(@RequestBody JsonNode notificationPayload) {
+        if (!midtransSignatureVerifier.isValidPaymentSignature(notificationPayload)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature");
+        }
+
+        // Process the notification payload
+        transactionService.handlePaymentNotification(notificationPayload);
+
+        // Return a response indicating success (200, no retries)
+        return ResponseEntity.status(HttpStatus.OK).body("Notification received successfully!!!");
+    }
+
+
+    @PostMapping("/recurring")
+    public ResponseEntity<String> handleIncomingRecurringNotification(@RequestBody Map<String, Object> notificationPayload) {
         // Log the received notification
-        logger.info("Received notification: {}", notificationPayload);
+        logger.info("Received recurring notification:\n{}", notificationPayload);
         // Process the notification payload as needed
 
         // Return a response indicating success (200, no retries)
         return ResponseEntity.status(HttpStatus.OK).body("Notification received successfully");
+    }
+
+    @PostMapping("/gopay-link")
+    public ResponseEntity<String> handleIncomingGoPayLinkingNotification(@RequestBody Object notificationPayload) {
+        // Log the received notification
+        logger.info("Received GoPay notification:\n{}", notificationPayload);
+        // Process the notification payload as needed
+
+        // Return a response indicating success (200, no retries)
+        return ResponseEntity.status(HttpStatus.OK).body("Notification received successfully");
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        logger.error("Error processing notification: {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
